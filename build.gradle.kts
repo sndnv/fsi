1e9537874fc1bbb8cc29b53a9863968765e55167
sndnv <sndnv@users.noreply.github.com>
2026-04-20 08:54:14 +0000

    Updating version to 1.1.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import java.nio.file.Paths
 
 group = "io.github.sndnv"
-version = "1.1.2"
+version = "1.1.3-SNAPSHOT"
 
 repositories {
     mavenCentral()
